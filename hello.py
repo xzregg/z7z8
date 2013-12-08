017e7a76adbbc8bb7b4917366d9ea6bb322a96e0
xzregg <xzregg@21cn.com>
2013-12-08 20:13:32 +0800

    nothing

--- hello.py
+++ hello.py
@@ -11,28 +11,45 @@ import logging
 import tornado.options
 import tornado.gen
 from tornado.options import define, options
+import threading
+from tornado.concurrent import run_on_executor
+
+
 
 settings = {'debug': True}#开启debug模式自动重启
 
 sys.path.insert(0,os.path.dirname(__file__))
+
+class testThread(threading.Thread):
+    def __init__(self,j):
+        super(testThread,self).__init__()
+        self.setDaemon(True)
+        self.j = j
+    def run(self):
+        for i in xrange(10):
+                self.j.write('%s<br>' % i)
+                time.sleep(1)
+                self.j.flush()
+        self.j.finish('ok')
+
+
+
 class MainHandler(tornado.web.RequestHandler):
 
+    #@tornado.web.asynchronous #取消自动finish
+    #@tornado.gen.engine
+    #@tornado.web.asynchronous
+    #@tornado.gen.coroutine
+
     @tornado.web.asynchronous
-    @tornado.gen.engine
     def get(self):
-            res = self.get_res()
+        args = self.get_argument('test','')
+        if args == '123':
+            t = testThread(self)
+            t.start()
+        else:
+            self.finish('asd')
 
-
-    @tornado.web.asynchronous
-    @tornado.gen.engine
-    def get_res(self):
-        print dir(self.request)
-        print self.request.uri
-        print
-
-        #res  = yield tornado.gen.Task()
-        #time.sleep(10)
-        self.finish('123')
 
     def callbacka(self,a='asd'):
 
@@ -52,5 +69,5 @@ def lee():
 if __name__ == '__main__':
     tornado.options.parse_command_line()
     application.listen(8888)
-    tornado.ioloop.PeriodicCallback(lee,3000).start()#增加后台任务
+    #tornado.ioloop.PeriodicCallback(lee,3000).start()#增加后台任务
     tornado.ioloop.IOLoop.instance().start()
