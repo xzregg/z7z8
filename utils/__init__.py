d8f61790fcd83e8555f0d1bd47e4123d7bb621a8
xzregg <xzregg@gmail.com>
2014-01-10 16:44:32 +0800

    t
    t

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,17 @@
+#coding:utf8
 
+from StringIO import StringIO
+import traceback
+import StringIO
+
+def get_traceback():
+    fp = StringIO.StringIO()
+    traceback.print_exc(file=fp)
+    message = fp.getvalue()
+    del fp
+    return message
+
+try:
+    raise IOError, "an i/o error occurred"
+except:
+    print get_traceback()
